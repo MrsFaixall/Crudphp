9fe70ecaad3aa37b8a1bcb63d7624fa12ff56b51
Faisal Mr <faisalmunadhilramzy@gmail.com>
2023-10-04 00:04:59 -0700

    Crudphp2

--- koneksi.php
+++ koneksi.php
@@ -15,12 +15,6 @@
 
     $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     
-    if($connection) {
-        echo "Koneksi Berhasil!";
-    } else {
-        echo "Koneksi Gagal! : ". mysqli_connect_error();
-    }
-    
     ?>
 </body>
 </html>
